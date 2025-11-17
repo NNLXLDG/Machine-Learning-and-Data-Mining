85edcc24b5009ec4c2783ec22213d1c9db779807
NNLXLDG <xxk1479670292@163.com>
2025-11-17 13:08:09 +0800

    update

--- Machine Learning Basic/helloworld.py
+++ Machine Learning Basic/helloworld.py
@@ -1,4 +1,27 @@
-import matplotlib.pyplot as plt
+import subprocess
 
-print(plt.rcParams)
-print(plt.rcParams['font.size'])
+# 基本用法
+result = subprocess.run(['ls'])
+
+# 捕获输出
+# result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+print(result)
+# print(result.returncode)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
